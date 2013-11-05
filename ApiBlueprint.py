8cc7ef3d2c189b85d9bf1a4d132b489272949fd9
autoscatto <4utoscatto@gmail.com>
2013-11-05 11:14:36 +0100

    python3 fix

--- ApiBlueprint.py
+++ ApiBlueprint.py
@@ -1,7 +1,6 @@
 import sublime
 import sublime_plugin
 import sys
-
 from os import path
 from subprocess import Popen, PIPE
 from sublime_plugin import TextCommand, WindowCommand
@@ -31,24 +30,26 @@ class Text():
 def run_command(cmd, args = [], source="", cwd = None, env = None):
   if not type(args) is list:
     args = [args]
-  
+
   if sys.platform == "win32":
     proc = Popen([cmd]+args, env=env, cwd=cwd, stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
-    stat = proc.communicate(input=source)
-  
+    stat = proc.communicate(input=source.encode('utf8'))
+
   else:
     if env is None:
       env = {"PATH": settings.get('binDir', '/usr/local/bin')}
 
     command = [cmd] + args
-    
+
     proc = Popen(command, env=env, cwd=cwd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    stat = proc.communicate(input=source)
-  
+
+    stat = proc.communicate(input=source.encode('utf8'))
+
   okay = proc.returncode == 0
-  
+
   #remove leading empty line
-  lines = stat[1].split('\n')
+  zstat = str( stat[1], encoding='utf8' )
+  lines = zstat.split('\n')
   if not lines[0]:
     lines.pop(0)
   sanitized_errout = '\n'.join(lines)
@@ -59,11 +60,82 @@ class ParseYamlAstCommand(TextCommand):
   def run(self, edit, **kwargs):
     opt = kwargs["opt"]
     args = opt
-    
     source = Text.all(self.view)
     result = run_command ("snowcrash", args=args, source=source)
-    
-    body = result["err"] + '\n' + result["out"]
+    body = "%s\n%s" % (result["err"],result["out"].decode("unicode_escape"))
+
+    output = self.view.window().new_file()
+    output.set_scratch(True)
+    output.set_syntax_file('Packages/YAML/YAML.tmLanguage')
+
+    if result["okay"] is True:
+      output.insert(edit, 0, body)
+    else:
+      output.insert(edit, 0, body)import sublime
+import sublime_plugin
+import sys
+from os import path
+from subprocess import Popen, PIPE
+from sublime_plugin import TextCommand, WindowCommand
+
+settings = sublime.load_settings('ApiBlueprint.sublime-settings')
+
+class Text():
+        @staticmethod
+        def all(view):
+                return view.substr(sublime.Region(0, view.size()))
+        @staticmethod
+        def sel(view):
+                text = []
+                for region in view.sel():
+                        if region.empty():
+                                continue
+                        text.append(view.substr(region))
+                return "".join(text)
+
+        @staticmethod
+        def get(view):
+                text = Text.sel(view)
+                if len(text) > 0:
+                        return text
+                return Text.all(view)
+
+def run_command(cmd, args = [], source="", cwd = None, env = None):
+  if not type(args) is list:
+    args = [args]
+
+  if sys.platform == "win32":
+    proc = Popen([cmd]+args, env=env, cwd=cwd, stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+    stat = proc.communicate(input=source.encode('utf8'))
+
+  else:
+    if env is None:
+      env = {"PATH": settings.get('binDir', '/usr/local/bin')}
+
+    command = [cmd] + args
+
+    proc = Popen(command, env=env, cwd=cwd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    stat = proc.communicate(input=source.encode('utf8'))
+
+  okay = proc.returncode == 0
+
+  #remove leading empty line
+  zstat = str( stat[1], encoding='utf8' )
+  lines = zstat.split('\n')
+  if not lines[0]:
+    lines.pop(0)
+  sanitized_errout = '\n'.join(lines)
+
+  return {"okay": okay, "out": stat[0], "err": sanitized_errout}
+
+class ParseYamlAstCommand(TextCommand):
+  def run(self, edit, **kwargs):
+    opt = kwargs["opt"]
+    args = opt
+    source = Text.all(self.view)
+    result = run_command ("snowcrash", args=args, source=source)
+    body = "%s\n%s" % (result["err"],result["out"].decode("unicode_escape"))
 
     output = self.view.window().new_file()
     output.set_scratch(True)
@@ -73,3 +145,4 @@ class ParseYamlAstCommand(TextCommand):
       output.insert(edit, 0, body)
     else:
       output.insert(edit, 0, body)
+
--- README.md
+++ README.md
@@ -1,42 +1,9 @@
 # API Blueprint Sublime Text plugin
 
 This plugin will help you write [API Blueprints](http://apiblueprint.org/) in your [favorite text editor](http://apiblueprint.org/) easier. 
+Please refer to [apiaryio api-blueprint-sublime-plugin](https://github.com/apiaryio/api-blueprint-sublime-plugin), this is a fork to make it work with python3 (sublime text 3) 
 
-## Requirements
 
-- [Install](http://apiblueprint.org/#get-started) API Blueprint command-line parser `snowcrash` first, please!
+## Changes
++ byte/string mess fix
 
-## Installation
-
-Sublime stores packages in the following locations:
-```
-Nix: ~/.config/sublime-text-2/packages
-Mac: ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
-Win: %APPDATA%\Sublime Text 2\Packages
-```
-Open a Terminal/Console and run the following commands, replacing `PACKAGE_PATH` with the path corresponding to your OS above.
-
-```
-cd PACKAGE_PATH
-git clone https://github.com/apiaryio/api-blueprint-sublime-plugin.git ApiBlueprint
-```
-
-## Commands/Shortcuts
-
-You can access the commands either using the command palette (`ctrl+shift+P` or `cmd+shift+P`) or via shortcuts.
-
-```
-alt+shift+b - Parse active file and open result in new tab 
-```
-
-# Notes
-
-**Ideas to do**
-
-- Syntax highlighting for blueprint
-- Syntax highliting for AST (yaml, json)
-- Show in browser preview of [Apiary](http://apiary.io/)interacitive Documentation
-- Generate and show static HTML documentation from [Iglo](https://github.com/subosito/iglo)
-- Show AST in JSON
-
-[Many thanks for inspiration](http://xavura.github.com/CoffeeScript-Sublime-Plugin)
